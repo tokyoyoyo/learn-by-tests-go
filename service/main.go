19930b5760c4813d36b1872b1156db8e8011b2c6
zhaojunjie <1278441739@qq.com>
2021-11-28 00:05:06 +0800

    将内存存储单列

--- service/main.go
+++ service/main.go
@@ -5,22 +5,6 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct {
-	store map[string]int
-}
-
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
-
 func main() {
 	server := &PlayerServer{NewInMemoryPlayerStore()}
 	if err := http.ListenAndServe(":8080", server); err != nil {
