4d6a042dfd9651e89155509dbde7156cdccb3abc
zhaojunjie <1278441739@qq.com>
2021-11-30 03:08:35 +0800

    通过测试，可以给胜者加分

--- service/CLI_test.go
+++ service/CLI_test.go
@@ -6,20 +6,18 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	in := strings.NewReader("Chris wins\n")
-	playerStore := &StubPlayerStore{}
-
-	cli := &CLI{playerStore, in}
-	cli.PlayPoker()
-
-	if len(playerStore.winCalls) != 1 {
-		t.Fatal("expected a win call but didn't get any")
-	}
-
-	got := playerStore.winCalls[0]
-	want := "Chris"
-
-	if got != want {
-		t.Errorf("didn't record correct winner, got %q, want %q", got, want)
-	}
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := &CLI{playerStore, in}
+		cli.PlayPoker()
+		assertPlayerWin(t, playerStore, "Chris")
+	})
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := &CLI{playerStore, in}
+		cli.PlayPoker()
+		assertPlayerWin(t, playerStore, "Cleo")
+	})
 }
--- service/server_test.go
+++ service/server_test.go
@@ -178,3 +178,15 @@ func assertContentType(t *testing.T, response *httptest.ResponseRecorder, want s
 		t.Errorf("response did not have content-type of %s, got %v", want, response.HeaderMap)
 	}
 }
+
+func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
+	t.Helper()
+
+	if len(store.winCalls) != 1 {
+		t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	}
+
+	if store.winCalls[0] != winner {
+		t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
+	}
+}
