081b2fcbf873d010422868324a315dab45968795
zhaojunjie <1278441739@qq.com>
2021-11-29 23:25:01 +0800

    实现了对League排序

--- service/file_system_store_test.go
+++ service/file_system_store_test.go
@@ -39,8 +39,8 @@ func TestFileSystemStore(t *testing.T) {
 		got := store.GetLeague()
 
 		want := []Player{
-			{"Cleo", 10},
 			{"Chris", 33},
+			{"Cleo", 10},
 		}
 
 		assertLeague(t, got, want)
@@ -102,6 +102,30 @@ func TestFileSystemStore(t *testing.T) {
 		defer cleanDatabase()
 		_, err := NewFileSystemPlayerStore(database)
 		assertNoError(t, err)
+	})
+
+	t.Run("league sorted", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+        {"Name": "Cleo", "Wins": 10},
+        {"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store, err := NewFileSystemPlayerStore(database)
+
+		assertNoError(t, err)
+
+		got := store.GetLeague()
+
+		want := []Player{
+			{"Chris", 33},
+			{"Cleo", 10},
+		}
+
+		assertLeague(t, got, want)
+
+		// read again
+		got = store.GetLeague()
+		assertLeague(t, got, want)
 	})
 }
 
